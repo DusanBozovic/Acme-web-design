e6405df9d06e9e47e70b726886bb7b92fe8375ec
Dusan Bozovic <dusan8888@gmail.com>
2018-06-03 22:26:08 +0300

    final entry

---
+++ app.js
@@ -0,0 +1,16 @@
+ Volume in drive C has no label.
+ Volume Serial Number is D2B6-B840
+
+ Directory of C:\Users\Dusan\Desktop\site
+
+02/11/2018  03:57 PM    <DIR>          .
+02/11/2018  03:57 PM    <DIR>          ..
+08/23/2017  05:31 PM             2,303 about.html
+02/11/2018  03:57 PM                 0 app.js
+08/23/2017  10:45 AM    <DIR>          img
+08/23/2017  05:31 PM             2,367 index.html
+08/23/2017  05:31 PM             2,727 services.html
+08/23/2017  05:31 PM             3,083 style.css
+               5 File(s)         10,480 bytes
+               3 Dir(s)  23,571,013,632 bytes free
+console.log("hiiii");
